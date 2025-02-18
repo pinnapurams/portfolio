46c6a89e33767a4c052000fa6b950a802226a2d7
Pinnapuram.Sasidhar <Pinnapuram.Sasidhar@mu-sigma.com>
2025-02-19 00:27:06 +0530

    Updated the Educations

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,20 +1,20 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor of Engineering",
+    title: "Bachelor of Engineering - CSE",
     duration: "2019 - 2023",
-    institution: "Sathyabama Instuite of science and technology,Chennai",
+    institution: "Annamacharya Instuite of Technology and Science, Tirupati",
   },
   {
     id: 2,
-    title: "Telangana State Board of Intermediate Education",
+    title: "Andhra Pradesh State Board of Intermediate Education",
     duration: "2017 - 2019",
-    institution: "Sri Chaithanaya Junior Kalasala,Hyderabad",
+    institution: "Sri Chaithanaya Junior College, Kadiri",
   },
   {
     id: 3,
-    title: "Board of Secondary Education,Telangana",
+    title: "Board of Secondary Education, Andhra Pradesh",
     duration: "2016 - 2017",
-    institution: "City Central School",
+    institution: "Sree Valmeeki E.M High School",
   }
 ]
