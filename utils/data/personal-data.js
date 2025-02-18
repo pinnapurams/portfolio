7e3b827dade64badbc264ac7f79f58bb943e7a63
Pinnapuram.Sasidhar <Pinnapuram.Sasidhar@mu-sigma.com>
2025-02-19 00:33:39 +0530

    Correct name spelling in the description

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Adapala Tejaswini",
   profile: '/profile.png',
   designation: "Software Developer",
-  description: "I am Adapala Tejaswin, a skilled Software Developer specializing in Front-End ReactJS Development and Java Backend Development with experience in building scalable SaaS applications. I hold a B.Tech in Computer Science from Annamacharya Institute of Technology (2019-2023) and have expertise in ReactJS, Java (Spring Boot), SQL, JavaScript, HTML, CSS, and RESTful API integration. My work focuses on developing custom eCommerce SaaS products, ensuring mobile responsiveness, and optimizing performance. I thrive in agile development environments, collaborating with teams using Git version control, and excel in problem-solving, troubleshooting, and UI/UX design implementation to create seamless user experiences.",
+  description: "I am Adapala Tejaswini, a skilled Software Developer specializing in Front-End ReactJS Development and Java Backend Development with experience in building scalable SaaS applications. I hold a B.Tech in Computer Science from Annamacharya Institute of Technology (2019-2023) and have expertise in ReactJS, Java (Spring Boot), SQL, JavaScript, HTML, CSS, and RESTful API integration. My work focuses on developing custom eCommerce SaaS products, ensuring mobile responsiveness, and optimizing performance. I thrive in agile development environments, collaborating with teams using Git version control, and excel in problem-solving, troubleshooting, and UI/UX design implementation to create seamless user experiences.",
   email: 'adapalatejaswini2001@gmail.com',
   phone: '+91 7671990259',
   address: 'Kadiri, Anantapur(dist), Andhra Pradesh, India',
